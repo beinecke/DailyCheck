04ae66abf05397f4049024566d8443c569da9548
Zhang Haiyan <zhanghaiyan@papayamobile.cn>
2022-07-08 15:05:33 +0800

    vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import App from './App.vue'
+
+const app = createApp(App)
+const vuetify = createVuetify(...) // Replaces new Vuetify(...)
+
+app.use(vuetify)
 
 createApp(App).use(router).mount('#app')
